01958e17cc2a486d8c29b9bbb7f86bd486b84f1e
warmtips <qkxyb@outlook.com>
2023-06-06 11:10:26 +0800

    9

--- themes/Acrylic/scripts/tags/note.js
+++
@@ -1,3 +0,0 @@
-hexo.extend.tag.register('note', function(type, content){
-    return `<div class="note ${type}">${hexo.render.renderSync({ text: content, engine: 'markdown' })}</div>`
-}, {ends: true});
